6402f690f25038d8615a46ce8c4813f4a8c0fe2a
israfil <israfil3019@gmail.com>
2021-08-02 23:58:59 +0300

    add url and view

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home
+from .views import about, home, teacher
 
 app_name = 'home'
 
 urlpatterns = [
     path('', home, name="home"),
-
+    path('about/', about, name="about"),
+    path('teacher/', teacher, name="teacher"),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "index.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def teacher(request):
+    return render(request, "teacher.html")
