eb28c84d161d549070bcf29443b023726c6d2e6f
israfil <israfil3019@gmail.com>
2021-08-03 11:11:24 +0300

    create user, change view, add form

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display  = ['name', 'phone_number', 'email', 'message']
+
+admin.site.register(Contact,ContactAdmin)
--- home/views.py
+++ home/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render
+from .forms import ContactForm
 
 
 def home(request):
-    return render(request, "index.html")
+
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            form = ContactForm()
+    else:
+        form =ContactForm()
+
+    context = {
+        'form' : form
+    }    
+
+
+    return render(request, 'index.html', context)
+
 
 def about(request):
-    return render(request, "about.html")
+    return render(request, 'about.html')
 
 def teacher(request):
-    return render(request, "teacher.html")
+    return render(request, 'teacher.html')
